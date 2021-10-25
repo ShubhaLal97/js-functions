4908749f303744e1207054d1d524090b6599aa85
shubha <shubha07658@gmail.com>
2021-10-25 23:19:12 +0530

    functions

---
+++ function.js
@@ -0,0 +1,22 @@
+//function numsum() {
+    //var num1 = 10
+    //var num2 = 20
+    //var sum = num1 + num2
+    //return sum
+    //console.log('test')
+//}
+
+//console.log(numsum())
+
+//function greetings(personName) {
+//console.log("welcome "+ personName)
+//}
+
+//greetings('shubha')
+//greetings('gullu')
+
+function totalSum(num1,num2, num3=30) {
+    console.log(num1+num2)
+}
+
+totalSum(5,5)
